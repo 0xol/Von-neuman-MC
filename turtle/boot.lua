32e713baa7f4e8c12457bf7ac2bed01de6bc49d6
0xol <86998622+0xol@users.noreply.github.com>
2022-12-18 18:41:10 +1000

    json stuff

--- turtle/boot.lua
+++ turtle/boot.lua
@@ -1,8 +1,8 @@
 local ws, err = http.websocket("ws://192.168.1.177:8863")
 
 while true do
-    sleep(5)
     ws.send(json.encode({id=os.getComputerLabel()}))
     func = assert(loadstring(ws.receive(5)))
     func()
+    sleep(1)
 end
