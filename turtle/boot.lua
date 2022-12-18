63b159ba71d9e0dd47116dbbe1d1086af99cbe7c
0xol <86998622+0xol@users.noreply.github.com>
2022-12-18 20:00:24 +1000

    updated boot.lua

--- turtle/boot.lua
+++ turtle/boot.lua
@@ -210,11 +210,8 @@ function decodeFromFile(path)
 	return decoded
 end
 
-local ws, err = http.websocket("ws://192.168.1.177:8863")
+local ws, err = http.websocket("ws://192.168.1.117:8863")
 
-while true do
-    ws.send(json.encode({id=os.getComputerLabel()}))
-    func = assert(loadstring(ws.receive(5)))
-    func()
-    sleep(1)
-end
+ws.send(encode({id=os.getComputerLabel()}))
+func = assert(loadstring(ws.receive(5)))
+func()
